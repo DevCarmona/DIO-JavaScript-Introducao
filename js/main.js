1979b07dd21d6a8885168e9b36d1f7135f0e0a0a
Andre da Silva Carmona <104648375+DevCarmona@users.noreply.github.com>
2022-12-05 09:16:49 -0300

    Array

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
+var lista = ["Maça", "Pêra", "Laranja"];
+//Adicionar item na lista
+lista.push("Uva")
+//Tira o último elemento da lista
+lista.pop();
+console.log(lista);
+console.log(lista[1]);
+//lenght = Quantidade de elementos que tem na lista (3)
+console.log(lista.length)
+//reverse = trás a lista de trás para frente (Laranja, Pêra, Maça)
+console.log(lista.reverse());
+//toString = Trás como String, perdendo a referência de Array
+console.log(lista.toString());
 
-var nome = "Andre Carmona";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo";
+//var nome = "Andre Carmona";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
 //alert(nome + " tem " + idade + " anos ");
 //alert(idade + idade2)
-console.log(nome);
-console.log(n1 + n2);
-console.log(n1 * n2);
-console.log(frase.toLowerCase());
-console.log(frase.replace("Japão", "Brasil"));
+//console.log(nome);
+//console.log(n1 + n2);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//console.log(frase.replace("Japão", "Brasil"));
